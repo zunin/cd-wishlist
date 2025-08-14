0d9213537866684a00306d8ee0557e227c5c77bf
Nikolai Øllegaard <nikolai@oellegaard.com>
2025-08-14 21:36:10 +0200

    fix(scraper): add scraper to wishlist

--- src/components/Wishlist.tsx
+++ src/components/Wishlist.tsx
@@ -9,25 +9,45 @@ import { type MusicbrainzMeta } from "../models/MusicbrainzMeta.ts";
 import { type Release } from "../models/Release.ts";
 
 export const Wishlist: FC<
-  { wishList: Array<string>; setWishList: (wishList: Array<string>) => void; releases: Array<Release> }
-> = ({ wishList, setWishList,releases }) => {
-  const [results, setResults] = useState([] as Array<{ musicBrainz: MusicbrainzMeta; available: Release[] }>);
-    const [musicBrainzClient] = useState(new MusicBrainzClient());
-    useEffect(() => {
-        async function fetch () {
-            const metaPromises = wishList.map(id => musicBrainzClient.getMusicBrainzHit(id));
-            const res = await Promise.all<MusicbrainzMeta>(metaPromises);
-            setResults(res.filter(x => !!x).map((meta) => {return {musicBrainz: meta, available: releases.filter(r => r.musicbrainz?.releaseGroupId === meta.releaseGroupId)}}))
-        }
-       
-        fetch();
+  {
+    wishList: Array<string>;
+    setWishList: (wishList: Array<string>) => void;
+    releases: Array<Release>;
+  }
+> = ({ wishList, setWishList, releases }) => {
+  const [results, setResults] = useState(
+    [] as Array<{ musicBrainz: MusicbrainzMeta; available: Release[] }>,
+  );
+  const [musicBrainzClient] = useState(new MusicBrainzClient());
+  useEffect(() => {
+    async function fetch() {
+      const metaPromises = wishList.map((id) =>
+        musicBrainzClient.getMusicBrainzHit(id)
+      );
+      const res = await Promise.all<MusicbrainzMeta>(metaPromises);
+      setResults(
+        res.filter((x) => !!x).map((meta) => {
+          return {
+            musicBrainz: meta,
+            available: releases.filter((r) =>
+              r.musicbrainz?.releaseGroupId === meta.releaseGroupId
+            ),
+          };
+        }),
+      );
+    }
 
-    }, [wishList, musicBrainzClient])
+    fetch();
+  }, [wishList, musicBrainzClient, releases]);
 
   return (
     <div>
       <h2>Wishlist</h2>
-      <AlbumArtistResultList wishList={wishList} setWishList={setWishList} results={results}>
+      <AlbumArtistResultList
+        wishList={wishList}
+        setWishList={setWishList}
+        results={results}
+      >
       </AlbumArtistResultList>
     </div>
   );
